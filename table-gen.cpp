e60a578f906712f9576b52da2784595b2468fe3e
Francesco Tosoni <39372510+francescotosoni@users.noreply.github.com>
2020-11-02 09:25:32 +0100

    update table-gen.cpp

--- table-gen.cpp
+++ table-gen.cpp
@@ -6,7 +6,6 @@ void make_crc_table_reverse(unsigned long crcTable[]) {
     unsigned long remainder;
     unsigned char b = 0;
     do {
-        // Start with the data byte
         remainder = b;
         for (unsigned long bit = 8; bit > 0; --bit) {
             if (remainder & 1)
@@ -20,24 +19,18 @@ void make_crc_table_reverse(unsigned long crcTable[]) {
 
 void make_crc_table_forward(unsigned long crcTable[]) {
     const uint polynomial = 0x1EDC6F41;
-    //crcTable = new uint[256];
 
-    for (int divident = 0; divident < 256; divident++) // iterate over all possible input byte values 0 - 255 
-    {
-        uint curByte = (uint)(divident << 24); // move divident byte into MSB of 32Bit CRC 
-        for (unsigned long bit = 0; bit < 8; bit++)
-        {
-            if ((curByte & 0x80000000) != 0)
-            {
+    for (int divident = 0; divident < 256; divident++) {
+        uint curByte = (uint)(divident << 24);  
+        for (unsigned long bit = 0; bit < 8; bit++) {
+            if ((curByte & 0x80000000) != 0) {
                 curByte <<= 1;
                 curByte ^= polynomial;
             }
-            else
-            {
+            else {
                 curByte <<= 1;
             }
         }
-
         crcTable[divident] = curByte;
     }
 } 

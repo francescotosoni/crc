e376d9f5c1f36e91c4abab44c762f5ee8a81e22b
Francesco Tosoni <39372510+francescotosoni@users.noreply.github.com>
2020-11-06 09:13:22 +0100

    parallela funzionante (non definitiva)

--- crc32-prl1.cu
+++ crc32-prl1.cu
@@ -1,15 +1,15 @@
+#include <iostream>
 #include <chrono>
 #include <iomanip>
-#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 #include <random>
 #include "tables.hpp"
-/*#include "Timer.cuh"
+#include "Timer.cuh"
 #include "CheckError.cuh"
 
-using namespace timer;*/
-
-const int BLOCK_SIZE_X = 16;
-const int BLOCK_SIZE_Y = 16;
+using namespace timer;
 
 __global__
 void crc32kernel(uint8_t* data, uint32_t* crc, int length, uint32_t* d_table, uint32_t* tmp) {
@@ -20,61 +20,88 @@ void crc32kernel(uint8_t* data, uint32_t* crc, int length, uint32_t* d_table, ui
     
     if(id < 4) { 
         uint32_t one = *current ^ *crc;
-        int i = (256*(15-id))+((one>>(8*id)) & 0xff);
+        int i = (256*((length-1)-id))+((one>>(8*id)) & 0xff);
         tmp[id] = d_table[i];
-        crc[0] = tmp[0];
     }
     else if(id > 3 && id < length) {
         uint32_t two = *(current+(id / 4));
-        int i = (256*(15-id))+(two>>(8*(id % 4)) & 0xff);
+        int i = (256*((length-1)-id))+(two>>(8*(id % 4)) & 0xff);
         tmp[id] = d_table[i];
     }
 
     __syncthreads();
 
-    if(id == 16) {
+    if(id == length+5) {
+        crc[0] = tmp[0];
         for(int i = 1; i < length; i++) {
-           crc[0] ^= (tmp[i]);
+            crc[0] ^= (tmp[i]);
         }
         crc[0] ^= 0xffffffff;
     }
 }
 
+__global__
+void xorkernel(uint32_t* v1, uint32_t* v2) {
+    *v1 ^= *v2;
+}
+
 int main() {
-    //Timer<DEVICE> TM_device;
-    //Timer<HOST>   TM_host;
+    Timer<DEVICE> TM_device;
+    Timer<HOST>   TM_host;
 
     // -------------------------------------------------------------------------
     // HOST MEMORY ALLOCATION
    
     uint32_t hcrc = 0xffffffff;
-    uint8_t data[] = "ciaobelocomestaisanhasbj";
-    int length = sizeof(data) - 1;
+    std::ifstream fin("input.txt");
+    std::string temp;
+    std::string d("");
 
+    while(fin.good()){
+        fin >> temp;
+        d.append(temp);
+    }
+    
+    std::cout << d.length() << std::endl;
+    uint8_t data[d.length()];
+
+    for(int i=0;i<d.length();i++){
+        data[i] = d[i] - '0';
+    }
+
+    int length = sizeof(data);
+    //printf("%d\n", length);
+
+    //uint32_t table[length][256];
+    auto *table = (uint32_t *)malloc(length * 256 * sizeof(uint32_t));
+    
+    make_crc_table_reverse(table, length);
+    
     // -------------------------------------------------------------------------
     // HOST INITILIZATION
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-    std::default_random_engine generator(seed);
     
     // -------------------------------------------------------------------------
     // HOST EXECUTIION
-    //TM_host.start();
+    TM_host.start();
+    //std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
 
     for(int i = 0; i < length; i++) {
-        hcrc = table_s[(hcrc ^ data[i]) & 0xFFL] ^ (hcrc>>8);
+        hcrc = table[(hcrc ^ data[i]) & 0xFF] ^ (hcrc>>8);
     }
 
     hcrc ^= 0xffffffff;
 
-    //TM_host.stop();
-    //TM_host.print("CRC32C host:   ");
-
+    TM_host.stop();
+    TM_host.print("CRC32C host:   ");
+    //std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    //double tempo = std::chrono::duration_cast<std::chrono::duration<double> >(end - start).count();
+    //std::cout << "Panato cpu time:" << tempo << std::endl;
     // -------------------------------------------------------------------------
     // DEVICE MEMORY ALLOCATION
-    //TM_device.start();
+    TM_device.start();
 
     uint32_t crc = 0xffffffff;
-    //uint8_t data[] = "ciaobelocomestai";
 
     uint32_t* dcrc;
     uint8_t* ddata;
@@ -82,23 +109,24 @@ int main() {
     uint32_t *tmp;
     cudaMalloc(&dcrc, length*sizeof(uint32_t));
     cudaMalloc(&ddata, length*sizeof(uint8_t));
-    cudaMalloc(&d_table, 16 * 256 * sizeof(uint32_t));
+    cudaMalloc(&d_table, length * 256 * sizeof(uint32_t));
     cudaMalloc(&tmp, length * sizeof(uint32_t));
 
+    
     // -------------------------------------------------------------------------
     // COPY DATA FROM HOST TO DEVICE
-    //SAFE_CALL(cudaMemcpy(dcrc, crc, sizeof(uint16_t), cudaMemcpyHostToDevice));
     cudaMemcpy(ddata, data, length*sizeof(uint8_t), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_table, table_m, 16 * 256 * sizeof(uint32_t), cudaMemcpyHostToDevice);
-
+    cudaMemcpy(d_table, table, length * 256 * sizeof(uint32_t), cudaMemcpyHostToDevice);
+    
     // -------------------------------------------------------------------------
     // DEVICE EXECUTION
     //TM_device.start();
 
-    dim3 block_size(256, 1, 1);
-    dim3 num_blocks(ceil((float)N/256), 1, 1);
+    dim3 block_size(1024, 1, 1);
+    dim3 num_blocks(ceil((float)length/1024), 1, 1);
    
     crc32kernel<<< num_blocks, block_size >>>(ddata, dcrc, length, d_table, tmp);
+    cudaDeviceSynchronize();
 
     /*TM_device.stop();
     CHECK_CUDA_ERROR
@@ -110,17 +138,16 @@ int main() {
 
     // -------------------------------------------------------------------------
     // COPY DATA FROM DEVICE TO HOST
-    cudaDeviceSynchronize();
     uint32_t h_crc_tmp;
     cudaMemcpy(&h_crc_tmp, dcrc, sizeof(uint32_t), cudaMemcpyDeviceToHost);
 
-    //TM_device.stop();
-    //CHECK_CUDA_ERROR
-    //TM_device.print("CRC32 device: ");
+    TM_device.stop();
+    CHECK_CUDA_ERROR
+    TM_device.print("CRC32 device: ");
 
-   /* std::cout << std::setprecision(1)
+    std::cout << std::setprecision(1)
               << "Speedup: " << TM_host.duration() / TM_device.duration()
-              << "x\n\n";*/
+              << "x\n\n";
 
     // -------------------------------------------------------------------------
     // RESULT CHECK

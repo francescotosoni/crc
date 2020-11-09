ef665330264f46e212b7ea494b040c34ec18c5fa
Francesco Tosoni <39372510+francescotosoni@users.noreply.github.com>
2020-11-09 16:40:15 +0100

    Update big-table-gen.cpp

--- parallel/big-table-gen.cpp
+++ parallel/big-table-gen.cpp
@@ -15,7 +15,7 @@ void make_crc_table_reverse(uint32_t* crc32table, int length) {
     
     for (int i = 0; i < 256; i++) {
         for (int slice = 1; slice < length; slice++) {
-        crc32table[slice*256+i] = (crc32table[(slice - 1)*256+i] >> 8) ^ crc32table[crc32table[(slice - 1)*256+i] & 0xFF];
+            crc32table[slice*256+i] = (crc32table[(slice - 1)*256+i] >> 8) ^ crc32table[crc32table[(slice - 1)*256+i] & 0xFF];
         }
     }
 }
